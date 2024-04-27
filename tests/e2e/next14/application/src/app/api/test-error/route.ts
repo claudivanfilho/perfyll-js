caab0c9d455c3b4bdbaf64fc1a2dbf887fe5a77f
Claudivan Filho <j.claudivan.filho@gmail.com>
2024-04-27 13:35:16 -0300

    update next14 api e2e tests

--- tests/e2e/next14/application/src/app/api/test-error/route.ts
+++ tests/e2e/next14/application/src/app/api/test-error/route.ts
@@ -1,19 +1,19 @@
-// import { logError } from "../../../../../../../../index";
-// import { initServer } from "../../../../../../../../server/index";
+import { logError } from "../../../../../../../../index";
+import { initServer } from "../../../../../../../../server/index";
 
-// initServer({
-//   secret: process.env.PERFYLL_SECRET!,
-//   publicKey: process.env.NEXT_PUBLIC_PERFYLL_PUBLIC_KEY!,
-//   forceHttp: true, // It must be true because we are dealing with an edge function
-//   serviceName: "my-next-server",
-//   customHttpUrl: process.env.NEXT_PUBLIC_PERFYLL_CUSTOM_API_URL,
-// });
+initServer({
+  secret: process.env.PERFYLL_SECRET!,
+  publicKey: process.env.NEXT_PUBLIC_PERFYLL_PUBLIC_KEY!,
+  forceHttp: true, // It must be true because we are dealing with an edge function
+  serviceName: "my-next-server",
+  customHttpUrl: process.env.NEXT_PUBLIC_PERFYLL_CUSTOM_API_URL,
+});
 
 export async function GET() {
   try {
     throw new Error("My next14 error test");
   } catch (error) {
-    // logError(error, { framework: "next14", mode: "backend" });
+    logError(error, { framework: "next14", mode: "backend" });
     return Response.json({ message: (error as Error).message }, { status: 500 });
   }
 }
